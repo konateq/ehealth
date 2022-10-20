384d1a5a8198ce2c7f359485f1e266281aea2147
Jerome SUBIGER <jerome.subiger@ext.ec.europa.eu>
2022-10-20 15:29:27 +0200

    EHEALTH-9165 Log level update

--- openncp-gateway/openncp-gateway-backend/src/main/java/eu/europa/ec/sante/ehdsi/openncp/gateway/module/eadc/persistence/model/EadcDbTimeConverter.java
+++ openncp-gateway/openncp-gateway-backend/src/main/java/eu/europa/ec/sante/ehdsi/openncp/gateway/module/eadc/persistence/model/EadcDbTimeConverter.java
@@ -7,8 +7,6 @@ import org.slf4j.LoggerFactory;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -17,8 +15,7 @@ public class EadcDbTimeConverter implements AttributeConverter<Instant, String> 
 
     private static final Logger logger = LoggerFactory.getLogger(EadcDbTimeConverter.class);
 
-    private  final String DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss Z";
-    private final ZoneId zoneId =  ZoneOffset.UTC; //ZoneId.systemDefault()
+    private static final String DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss Z";
 
     @Override
     public String convertToDatabaseColumn(Instant instant) {
@@ -29,8 +26,8 @@ public class EadcDbTimeConverter implements AttributeConverter<Instant, String> 
 
     @Override
     public Instant convertToEntityAttribute(String s) {
-        if(StringUtils.isBlank(s)) {
-            logger.warn("date field is not populated!");
+        if (StringUtils.isBlank(s)) {
+            logger.debug("date field is not populated!");
             return null;
         }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
@@ -39,7 +36,7 @@ public class EadcDbTimeConverter implements AttributeConverter<Instant, String> 
             timed.format(formatter);
             return Instant.from(timed);
         } catch (RuntimeException ex) {
-            logger.warn(ex.getMessage());
+            logger.error(ex.getMessage());
             return null;
         }
     }
