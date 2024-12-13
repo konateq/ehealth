f5bb9e3d8c88ceb7462f2215a0111d74830ba11c
Mathias Ghys <mathias.ghys@ext.ec.europa.eu>
2024-12-13 17:16:31 +0100

    [EHEALTH-12985] Update Dicom KOS dcm file

--- openncp-api/openncp-api-server/src/test/java/eu/europa/ec/sante/openncp/api/server/dicom/DicomExtractorTest.java
+++ openncp-api/openncp-api-server/src/test/java/eu/europa/ec/sante/openncp/api/server/dicom/DicomExtractorTest.java
@@ -12,17 +12,16 @@ import java.util.List;
 public class DicomExtractorTest {
 
     @Test
-    public void testExtractDicomStudy() throws URISyntaxException, IOException {
-        InputStream dicomKosInputStream = this.getClass().getResourceAsStream("/medicalimages/1000_1010_KOS.dcm");
+    public void testExtractDicomStudy() throws IOException {
+        InputStream dicomKosInputStream = this.getClass().getResourceAsStream("/medicalimages/dicomKos.dcm");
         DicomExtractor dicomExtractor = new DicomExtractor();
         DicomStudy dicomStudy = dicomExtractor.extractDicomStudy(dicomKosInputStream);
-        Assert.assertEquals("1.3.6.1.4.1.14519.5.2.1.7085.2626.192997540292073877946622133586", dicomStudy.getUID());
+        Assert.assertEquals("1.2.276.0.7230010.3.1.2.296485376.1.1521713414.1800996", dicomStudy.getUID());
         List<DicomSeries> dicomSeries = dicomStudy.getSeries();
         Assert.assertEquals(1, dicomSeries.size());
         DicomSeries series = dicomSeries.get(0);
-        Assert.assertEquals("1.3.6.1.4.1.14519.5.2.1.7085.2626.328191285537072639441393834220", series.getUID());
+        Assert.assertEquals("1.2.276.0.7230010.3.1.3.296485376.1.1521713419.1802493", series.getUID());
         List<DicomInstance> dicomInstances = series.getInstances();
         Assert.assertEquals(11, dicomInstances.size());
-        System.out.println(dicomStudy);
     }
 }
