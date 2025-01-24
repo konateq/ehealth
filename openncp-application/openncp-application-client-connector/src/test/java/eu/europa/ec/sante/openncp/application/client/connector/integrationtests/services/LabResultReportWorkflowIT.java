61de7a4e9751ff030dae62584be602b13eda9dd9
Kim Wauters <kim.wauters@ext.ec.europa.eu>
2025-01-24 16:20:08 +0100

    [EHEALTH-13200] Create labresult report integration tests

---
+++ openncp-application/openncp-application-client-connector/src/test/java/eu/europa/ec/sante/openncp/application/client/connector/integrationtests/services/LabResultReportWorkflowIT.java
@@ -0,0 +1,57 @@
+package eu.europa.ec.sante.openncp.application.client.connector.integrationtests.services;
+
+import eu.europa.ec.sante.openncp.application.client.connector.ClientConnectorException;
+import eu.europa.ec.sante.openncp.application.client.connector.ClientConnectorService;
+import eu.europa.ec.sante.openncp.application.client.connector.assertion.AssertionService;
+import eu.europa.ec.sante.openncp.application.client.connector.integrationtests.util.AssertionUtils;
+import eu.europa.ec.sante.openncp.common.configuration.ConfigurationManager;
+import eu.europa.ec.sante.openncp.common.security.AssertionType;
+import eu.europa.ec.sante.openncp.common.security.key.KeyStoreManager;
+import eu.europa.ec.sante.openncp.core.client.api.ObjectFactory;
+import eu.europa.ec.sante.openncp.core.client.api.PatientId;
+import org.junit.jupiter.api.Test;
+import org.opensaml.saml.saml2.core.Assertion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LabResultReportWorkflowIT extends BaseIntegrationTest {
+
+    @Autowired
+    private AssertionService assertionService;
+
+    @Autowired
+    private ConfigurationManager configurationManager;
+
+    @Autowired
+    private ClientConnectorService clientConnectorService;
+
+    @Autowired
+    private KeyStoreManager keyStoreManager;
+
+    private final ObjectFactory objectFactory = new ObjectFactory();
+
+    @Test
+    void queryBundle() throws ClientConnectorException {
+        final Map<AssertionType, Assertion> assertions = new HashMap<>();
+        final Assertion clinicalAssertion = AssertionUtils.createClinicalAssertion(keyStoreManager, "Doctor House", "John House", "house@ehdsi.eu");
+        assertions.put(AssertionType.HCP, clinicalAssertion);
+
+        final PatientId patientId = objectFactory.createPatientId();
+        patientId.setRoot("https://www.ehealth.fgov.be/standards/fhir/core/NamingSystem/ssin");
+        patientId.setExtension("89121210976");
+        final Map<String, String> params = Map.of(
+                "identifier", patientId.getRoot() + "|" + patientId.getExtension(),
+                "type", "http://loinc.org|18719-5");
+
+        final ResponseEntity<String> responseEntity = clientConnectorService.queryBundleFhir(assertions, "BE", params);
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode().is2xxSuccessful()).isTrue();
+
+    }
+
+}
