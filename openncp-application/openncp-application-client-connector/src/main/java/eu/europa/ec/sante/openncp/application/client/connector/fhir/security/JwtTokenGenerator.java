66c5d1a09d96154a04d65478e5c6eb9c89bccddf
Kim Wauters <kim.wauters@ext.ec.europa.eu>
2025-01-31 14:23:36 +0100

    [EHEALTH-12573] No TRC assertion used for the HL7 FHIR flows   - Fix for the failing TRC validation for the FHIR flows

--- openncp-application/openncp-application-client-connector/src/main/java/eu/europa/ec/sante/openncp/application/client/connector/fhir/security/JwtTokenGenerator.java
+++ openncp-application/openncp-application-client-connector/src/main/java/eu/europa/ec/sante/openncp/application/client/connector/fhir/security/JwtTokenGenerator.java
@@ -8,8 +8,13 @@ import eu.europa.ec.sante.openncp.common.configuration.ConfigurationManager;
 import eu.europa.ec.sante.openncp.common.security.AssertionType;
 import net.shibboleth.utilities.java.support.xml.SerializeSupport;
 import org.apache.commons.lang.time.DateUtils;
+import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
+import org.opensaml.core.xml.io.Marshaller;
+import org.opensaml.core.xml.io.MarshallerFactory;
+import org.opensaml.core.xml.io.MarshallingException;
 import org.opensaml.saml.saml2.core.Assertion;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Element;
 
 import java.util.Base64;
 import java.util.Date;
@@ -27,27 +32,43 @@ public class JwtTokenGenerator {
 
     public String generate(final Map<AssertionType, Assertion> assertions) {
 
-        Assertion hcpAssertion = assertions.get(AssertionType.HCP);
-        Assertion trcAssertion = assertions.get(AssertionType.TRC);
-        Assertion nokAssertion = assertions.get(AssertionType.NOK);
+        final Assertion hcpAssertion = assertions.get(AssertionType.HCP);
+        final Assertion trcAssertion = assertions.get(AssertionType.TRC);
+        final Assertion nokAssertion = assertions.get(AssertionType.NOK);
 
-        final String hcp = SerializeSupport.prettyPrintXML(hcpAssertion.getDOM());
+        final String hcp = serializeAssertionToXML(hcpAssertion);
         final Base64.Encoder encoder = Base64.getEncoder();
 
-        JWTCreator.Builder jwtBuilder = JWT.create()
+        final JWTCreator.Builder jwtBuilder = JWT.create()
                 .withSubject("user")
-                .withExpiresAt(DateUtils.addHours(new Date(), 24))
+                .withExpiresAt(DateUtils.addHours(new Date(), 1))
                 .withClaim(AssertionType.HCP.name(), encoder.encodeToString(hcp.getBytes()));
 
         if (trcAssertion != null) {
-            final String trc = SerializeSupport.prettyPrintXML(trcAssertion.getDOM());
+            final String trc = serializeAssertionToXML(trcAssertion);
             jwtBuilder.withClaim(AssertionType.TRC.name(), encoder.encodeToString(trc.getBytes()));
         }
 
         if (nokAssertion != null) {
-            final String nok = SerializeSupport.prettyPrintXML(nokAssertion.getDOM());
+            final String nok = serializeAssertionToXML(nokAssertion);
             jwtBuilder.withClaim(AssertionType.NOK.name(), encoder.encodeToString(nok.getBytes()));
         }
         return jwtBuilder.sign(Algorithm.HMAC512(configurationManager.getProperty(Constant.JWT_SECRET)));
+    }
+
+    public static String serializeAssertionToXML(final Assertion assertion) {
+        // Obtain a Marshaller
+        final MarshallerFactory marshallerFactory = XMLObjectProviderRegistrySupport.getMarshallerFactory();
+        final Marshaller marshaller = marshallerFactory.getMarshaller(assertion);
+
+        // Marshall the assertion into a DOM Element
+        final Element element;
+        try {
+            element = marshaller.marshall(assertion);
+        } catch (final MarshallingException e) {
+            throw new RuntimeException("Exception when marshalling the assertion", e);
+        }
+
+        return SerializeSupport.nodeToString(element);
     }
 }
