9264cf303f3aa74d0d3eaf4d99b7fa166254c6df
Mathias Ghys <mathias.ghys@ext.ec.europa.eu>
2025-03-12 12:03:38 +0100

    [EHEALTH-13528] Fix issue with partially generated AuditMessage when the event.getSP_UserID is empty + Make the audit logging more robust with better error propagation.

--- openncp-common/src/main/java/eu/europa/ec/sante/openncp/common/audit/auditmessagebuilders/TRCAssertionAuditMessageBuilder.java
+++ openncp-common/src/main/java/eu/europa/ec/sante/openncp/common/audit/auditmessagebuilders/TRCAssertionAuditMessageBuilder.java
@@ -14,36 +14,29 @@ public class TRCAssertionAuditMessageBuilder extends AbstractAuditMessageBuilder
 
     @Override
     public AuditMessage build(final EventLog eventLog) {
-        AuditMessage message = null;
-        try {
-            final ObjectFactory of = new ObjectFactory();
-            message = of.createAuditMessage();
-            // Audit Source
-            addAuditSource(message, eventLog.getAS_AuditSourceId());
-            // Event Identification
-            addEventIdentification(message, eventLog.getEventType(), eventLog.getEI_TransactionName(), EventActionCode.EXECUTE.getCode(),
-                    eventLog.getEI_EventDateTime(), eventLog.getEI_EventOutcomeIndicator(), eventLog.getNcpSide());
-            // Point Of Care
-            addPointOfCare(message, eventLog.getPC_UserID(), eventLog.getSourceip());
-            // Human Requestor
-            addHumanRequestor(message, eventLog.getHR_UserID(), eventLog.getHR_AlternativeUserID(), eventLog.getHR_RoleID(),
-                    true, eventLog.getSourceip());
-            addService(message, eventLog.getSC_UserID(), true, AuditConstant.SERVICE_CONSUMER, AuditConstant.CODE_SYSTEM_EHDSI, AuditConstant.SERVICE_CONSUMER_DISPLAY_NAME,
-                    eventLog.getSourceip());
-            addService(message, eventLog.getSP_UserID(), false, AuditConstant.SERVICE_PROVIDER, AuditConstant.CODE_SYSTEM_EHDSI, AuditConstant.SERVICE_PROVIDER_DISPLAY_NAME,
-                    eventLog.getTargetip());
-            for (final String ptParticipantObjectID : eventLog.getPT_ParticipantObjectIDs()) {
-                addParticipantObject(message, ptParticipantObjectID, Short.valueOf("1"), Short.valueOf("1"), "PatientSource",
-                        "2", AuditConstant.RFC3881, "Patient Number",
-                        "Patient Number", eventLog.getQueryByParameter(), eventLog.getHciIdentifier());
-            }
-        } catch (final Exception e) {
-            LOGGER.error(e.getLocalizedMessage(), e);
+        final ObjectFactory of = new ObjectFactory();
+        AuditMessage message = of.createAuditMessage();
+        // Audit Source
+        addAuditSource(message, eventLog.getAS_AuditSourceId());
+        // Event Identification
+        addEventIdentification(message, eventLog.getEventType(), eventLog.getEI_TransactionName(), EventActionCode.EXECUTE.getCode(),
+                eventLog.getEI_EventDateTime(), eventLog.getEI_EventOutcomeIndicator(), eventLog.getNcpSide());
+        // Point Of Care
+        addPointOfCare(message, eventLog.getPC_UserID(), eventLog.getSourceip());
+        // Human Requestor
+        addHumanRequestor(message, eventLog.getHR_UserID(), eventLog.getHR_AlternativeUserID(), eventLog.getHR_RoleID(),
+                true, eventLog.getSourceip());
+        addService(message, eventLog.getSC_UserID(), true, AuditConstant.SERVICE_CONSUMER, AuditConstant.CODE_SYSTEM_EHDSI, AuditConstant.SERVICE_CONSUMER_DISPLAY_NAME,
+                eventLog.getSourceip());
+        addService(message, eventLog.getSP_UserID(), false, AuditConstant.SERVICE_PROVIDER, AuditConstant.CODE_SYSTEM_EHDSI, AuditConstant.SERVICE_PROVIDER_DISPLAY_NAME,
+                eventLog.getTargetip());
+        for (final String ptParticipantObjectID : eventLog.getPT_ParticipantObjectIDs()) {
+            addParticipantObject(message, ptParticipantObjectID, Short.valueOf("1"), Short.valueOf("1"), "PatientSource",
+                    "2", AuditConstant.RFC3881, "Patient Number",
+                    "Patient Number", eventLog.getQueryByParameter(), eventLog.getHciIdentifier());
         }
-        if (message != null) {
-            addEventTarget(message, eventLog.getEventTargetParticipantObjectIds(), Short.valueOf("2"), null,
-                    "TrcA", AuditConstant.CODE_SYSTEM_EHDSI_SECURITY, "TRC Assertion");
-        }
+        addEventTarget(message, eventLog.getEventTargetParticipantObjectIds(), Short.valueOf("2"), null,
+                "TrcA", AuditConstant.CODE_SYSTEM_EHDSI_SECURITY, "TRC Assertion");
         return message;
     }
 }
