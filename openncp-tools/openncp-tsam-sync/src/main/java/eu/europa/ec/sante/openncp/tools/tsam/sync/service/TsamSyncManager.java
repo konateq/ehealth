01953f79de1df4059dc05ad6b3d61637735f7bcc
miorial <alberto.miori@ext.ec.europa.eu>
2025-03-27 18:40:00 +0100

    HEALTH-11510 TSAM Issue

--- openncp-tools/openncp-tsam-sync/src/main/java/eu/europa/ec/sante/openncp/tools/tsam/sync/service/TsamSyncManager.java
+++ openncp-tools/openncp-tsam-sync/src/main/java/eu/europa/ec/sante/openncp/tools/tsam/sync/service/TsamSyncManager.java
@@ -1,18 +1,14 @@
 package eu.europa.ec.sante.openncp.tools.tsam.sync.service;
 
-import com.ibatis.common.jdbc.ScriptRunner;
 import eu.europa.ec.sante.openncp.tools.tsam.sync.config.DatabaseProperties;
 import eu.europa.ec.sante.openncp.tools.tsam.sync.converter.ConceptConverter;
 import eu.europa.ec.sante.openncp.tools.tsam.sync.converter.ValueSetVersionConverter;
-import eu.europa.ec.sante.openncp.tools.tsam.sync.domain.ValueSetVersion;
+import eu.europa.ec.sante.openncp.tools.tsam.sync.domain.*;
 import eu.europa.ec.sante.openncp.tools.tsam.sync.domainehealthproperty.model.Property;
 import eu.europa.ec.sante.openncp.tools.tsam.sync.repository.*;
 import eu.europa.ec.sante.openncp.tools.tsam.sync.config.CtsProperties;
 import eu.europa.ec.sante.openncp.tools.tsam.sync.cts.CtsClient;
 import eu.europa.ec.sante.openncp.tools.tsam.sync.db.DatabaseTool;
-import eu.europa.ec.sante.openncp.tools.tsam.sync.domain.Concept;
-import eu.europa.ec.sante.openncp.tools.tsam.sync.domain.Mapping;
-import eu.europa.ec.sante.openncp.tools.tsam.sync.domain.Designation;
 import eu.europa.ec.sante.openncp.tools.tsam.sync.domainehealthproperty.service.PropertyService;
 import eu.europa.ec.sante.ehdsi.termservice.web.rest.model.sync.ValueSetCatalogModel;
 import eu.europa.ec.sante.ehdsi.termservice.web.rest.model.sync.ValueSetVersionModel;
@@ -24,20 +20,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StopWatch;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-import java.io.StringReader;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -55,14 +40,17 @@ public class TsamSyncManager {
     private final MappingRepository mappingRepository;
     private final ConceptConverter conceptConverter;
     private final PropertyService propertyService;
-    private static String databaseProduct = "";
-    private static final String JDBC_EHNCP_PROPERTY = "jdbc/ehealth_ltrdb";
-    private static String sqlCleanLocalDb;
+    private static Boolean extraVerboseLogging = false;
 
     private final ValueSetVersionConverter valueSetVersionConverter;
+    private final DesignationRepository designationRepository;
+    private final CodeSystemVersionRepository codeSystemVersionRepository;
 
-    @Value("${pageable.page-size:500}")
+    @Value("${pageable.page-size:250}")
     private Integer pageSize;
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
     private static final String AVAILABLE_TRANSLATION_LANGUAGES_PROPERTY_KEY = "AVAILABLE_TRANSLATION_LANGUAGES";
 
@@ -71,7 +59,9 @@ public class TsamSyncManager {
                            DatabaseProperties databaseProperties, CodeSystemRepository codeSystemRepository,
                            ConceptRepository conceptRepository, MappingRepository mappingRepository,
                            ValueSetRepository valueSetRepository, ValueSetVersionRepository valueSetVersionRepository,
-                           ConceptConverter conceptConverter, PropertyService propertyService, ValueSetVersionConverter valueSetVersionConverter) {
+                           ConceptConverter conceptConverter, PropertyService propertyService,
+                           ValueSetVersionConverter valueSetVersionConverter, DesignationRepository designationRepository,
+                           CodeSystemVersionRepository codeSystemVersionRepository) {
         this.databaseTool = databaseTool;
         this.ctsProperties = ctsProperties;
         this.ctsClient = ctsClient;
@@ -84,6 +74,8 @@ public class TsamSyncManager {
         this.conceptConverter = conceptConverter;
         this.propertyService = propertyService;
         this.valueSetVersionConverter = valueSetVersionConverter;
+        this.designationRepository = designationRepository;
+        this.codeSystemVersionRepository = codeSystemVersionRepository;
     }
 
     @Transactional
@@ -93,6 +85,8 @@ public class TsamSyncManager {
         logger.info("Authenticating user '{}'", ctsProperties.getUsername());
         ctsClient.authenticate();
         logger.info("User '{}' authenticated successfully to the Central Terminology Services ({})", ctsProperties.getUsername(), ctsProperties.getUrl());
+
+        logger.info("Active profile: '{}'", activeProfile);
 
         logger.info("Checking for updates");
         Optional<ValueSetCatalogModel> opt = ctsClient.fetchCatalogue();
@@ -112,22 +106,55 @@ public class TsamSyncManager {
                 logger.warn("Database backup is disabled (Property 'tsam-sync.database.backup' is set to 'false')");
             }
 
-            if(databaseProduct.isEmpty()) {
-                retrieveDbProperties();
-            }
-
             // Cleaning current LTR Database.
             logger.info("Cleaning current LTR Database");
-            try {
-                truncateDatabaseTables();
-            } catch (Exception e) {
-                logger.info("Cleaning Mappings");
-                mappingRepository.deleteAll();
-                logger.info("Cleaning ValueSets");
-                valueSetRepository.deleteAll();
-                logger.info("Cleaning CodeSystems");
-                codeSystemRepository.deleteAll();
+
+            logger.info("Cleaning Mappings...");
+            mappingRepository.deleteAllInBatch();
+            stopWatch.stop();
+            logger.info("Cleaned Mapppings done! ({} ms)", stopWatch.getTotalTimeMillis());
+            stopWatch.start();
+
+            logger.info("Cleaning ValueSetsVersions...");
+            valueSetVersionRepository.deleteAllInBatch();
+            stopWatch.stop();
+            logger.info("Cleaned ValueSetsVersions done! ({} ms)", stopWatch.getTotalTimeMillis());
+            stopWatch.start();
+
+            logger.info("Cleaning ValueSets...");
+            valueSetRepository.deleteAllInBatch();
+            stopWatch.stop();
+            logger.info("Cleaned ValueSets done! ({} ms)", stopWatch.getTotalTimeMillis());
+            stopWatch.start();
+
+            logger.info("Cleaning Designations...");
+            designationRepository.deleteAllInBatch();
+            stopWatch.stop();
+            logger.info("Cleaned Designations done! ({} ms)", stopWatch.getTotalTimeMillis());
+            stopWatch.start();
+
+            if("postgresql".equals(activeProfile)) {
+                logger.info("Cleaning Concepts (Postgres)...");
+                conceptRepository.truncateTable();
+            } else {
+                logger.info("Cleaning Concepts...");
+                conceptRepository.deleteAllInBatch();
             }
+            stopWatch.stop();
+            logger.info("Cleaning Concepts done!({} ms)", stopWatch.getTotalTimeMillis());
+            stopWatch.start();
+
+            logger.info("Cleaning CodeSystemVersions...");
+            codeSystemVersionRepository.deleteAllInBatch();
+            stopWatch.stop();
+            logger.info("Cleaning CodeSystemVersions done! ({} ms)", stopWatch.getTotalTimeMillis());
+            stopWatch.start();
+
+            logger.info("Cleaning CodeSystems...");
+            codeSystemRepository.deleteAllInBatch();
+            stopWatch.stop();
+            logger.info("Cleaning CodeSystems done! ({} ms)", stopWatch.getTotalTimeMillis());
+            stopWatch.start();
 
             logger.info("Starting value sets synchronization");
             int index = 1;
@@ -164,20 +191,24 @@ public class TsamSyncManager {
                             .map(conceptConverter::convert)
                             .collect(Collectors.toList());
 
-                    logger.info("   fetch {} concepts...", concepts.size());
+                    int base = ((page - 1) * getPageSize());
+                    logger.info("   fetch concepts from {} to {}...", base, (base + concepts.size()));
 
-                    concepts.forEach(concept -> concept.addValueSetVersion(valueSetVersion));
                     for (Concept concept : concepts) {
+                        concept.addValueSetVersion(valueSetVersion);
+
                         numberOfTotalMappings += concept.getMappings().size();
 
-                        logger.info("       concept {} - {} -> {} mappings found",
-                                concept.getCode(), concept.getDefinition(), concept.getMappings().size());
+                        if(extraVerboseLogging) {
+                            logger.info("       concept {} - {} -> {} mappings found",
+                                    concept.getCode(), concept.getDefinition(), concept.getMappings().size());
 
-                        for(Mapping mapping : concept.getMappings()) {
-                            logger.info("           mapping source {}:{} - target {}:{} - status {}",
-                                    mapping.getSource().getCode(), mapping.getSource().getDefinition(),
-                                    mapping.getTarget().getCode(), mapping.getTarget().getDefinition(),
-                                    mapping.getStatus());
+                            for (Mapping mapping : concept.getMappings()) {
+                                logger.info("           mapping source {}:{} - target {}:{} - status {}",
+                                        mapping.getSource().getCode(), mapping.getSource().getDefinition(),
+                                        mapping.getTarget().getCode(), mapping.getTarget().getDefinition(),
+                                        mapping.getStatus());
+                            }
                         }
 
                         for(Designation designation : concept.getDesignations()) {
@@ -214,57 +245,4 @@ public class TsamSyncManager {
     public void setPageSize(Integer pageSize){
         this.pageSize = pageSize;
     }
-
-    private Connection dbConnect(String dsName) throws NamingException, SQLException {
-        Context initContext = new InitialContext();
-        Context envContext = (Context) initContext.lookup("java:/comp/env");
-        DataSource ds = (DataSource) envContext.lookup(dsName);
-        return ds.getConnection();
-    }
-
-    private void retrieveDbProperties()
-    {
-        logger.info("Retrieving db properties");
-        try (Connection sqlConnection = dbConnect(TsamSyncManager.JDBC_EHNCP_PROPERTY);
-             Statement stmt = sqlConnection.createStatement()) {
-            databaseProduct = sqlConnection.getMetaData().getDatabaseProductName().toLowerCase();
-        } catch (Exception exception) {
-           // if (OpenNCPConstants.NCP_SERVER_MODE != ServerMode.PRODUCTION && loggerClinical.isDebugEnabled()) {
-            //   loggerClinical.error("Cannot detect database type");
-            //}
-        }
-        logger.info("Database is : {}", databaseProduct);
-    }
-
-    private void runSqlScript(String sqlScript) throws Exception {
-
-        logger.info("Running sql script : {}", sqlScript);
-
-        try (Connection sqlConnection = dbConnect(TsamSyncManager.JDBC_EHNCP_PROPERTY);
-             StringReader stringReader = new StringReader(sqlScript)) {
-
-            ScriptRunner objScriptRunner = new ScriptRunner(sqlConnection, false, true);
-            objScriptRunner.setLogWriter(null);
-            objScriptRunner.setErrorLogWriter(null);
-            objScriptRunner.runScript(stringReader);
-
-        } catch (Exception exception) {
-            throw new Exception("The following error occurred during an SQL operation:", exception);
-        }
-    }
-
-    private void truncateDatabaseTables() throws Exception {
-        logger.info("Truncate database");
-        runSqlScript("SET FOREIGN_KEY_CHECKS = 0;");
-        runSqlScript("TRUNCATE TABLE designation;");
-        runSqlScript("TRUNCATE TABLE transcoding_association;");
-        runSqlScript("TRUNCATE TABLE x_concept_value_set;");
-        runSqlScript("TRUNCATE TABLE code_system_concept;");
-        runSqlScript("TRUNCATE TABLE code_system_version;");
-        runSqlScript("TRUNCATE TABLE code_system;");
-        runSqlScript("TRUNCATE TABLE value_set_version;");
-        runSqlScript("TRUNCATE TABLE value_set;");
-        runSqlScript("SET FOREIGN_KEY_CHECKS = 1;");
-    }
-
 }
